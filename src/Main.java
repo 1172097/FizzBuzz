d450b708e36cff9d74fda1a2e3f8bff63592245e
Andy Ren <andyren@Andys-MacBook-Pro-2.local>
2023-09-11 12:37:39 -0400

    refactored to extract helper method

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] strarray = {"5"};
+        FizzBuzz.main(strarray);
+        FizzBuzz.doFizzBuzz(5);
+
+
+    }
+}
